cbf4c5197e2d646bf4e564f2cbb75879a0d67f24
drewpj <drewjacobson@live.com>
2018-04-03 00:32:54 -0400

    Completed proof of concept of how to gather tweets. It's currently 12:30am and I'm too tired to do the rest of the program right now. I've included a test file.

--- searchTweets.py
+++ searchTweets.py
@@ -1,21 +1,62 @@
 #import networkx as nx
 #import matplotlib.pyplot as plt
 import json
+import pprint
 from TwitterModule import *
-
+import time
 #Set up api and global variables
 twitter_api = oauth_login()#twitter api for grabbing data
 tweetsIngraham = {}
-
+tweetsIngrahamList = []
+ingrahamSearch = open('ingrahamSearch.json','w')
 q = 'IngrahamAngle'
-#statuses = twitter_search(twitter_api.search.tweets,q, max_results=10, until=2018-03-27)
-#statuses = make_twitter_request(twitter_api.search.tweets,q, max_results=10, until=2018-03-27)
-#print statuses
 
-#q = "CrossFit"
-for
-results = twitter_search(twitter_api, q, result_type='recent',max_results=10)
-resultsJSON = json.loads(results)
-tweetsIngraham.append(resultsJSON['statuses'])
-#print json.dumps(results['statuses'],indent-1)
-#print json.dumps(results[0], indent=1)
+
+'''
+First search call to twitter_api
+Parameters:
+    q: is the search term
+    result_type: is whether we want recent, popular or mixed tweets. Currently set to recent
+    max_results: is how many results we wan to take in a single call. Is currently 10 for testing
+    until: specifies the date that all tweets returned form this call should come before
+            (so all tweets from this call are from 3/28/2018)
+
+getMaxID parses the maxID from the appropriate string in the search return metadata
+maxid will then be used to call the next batch of tweets. More info on maxid is Availableon the search api documentation
+'''
+response = make_twitter_request(twitter_api.search.tweets,q=q,result_type='recent',max_results=10, until = '2018-03-29')
+next_results = response['search_metadata']['next_results']
+getMaxID = dict([ kv.split('=') for kv in next_results[1:].split("&") ])
+maxid = getMaxID['max_id']
+
+
+'''
+Currently only running 10 times for testing. Figuring out how often to run and how to stop it from running will be a challenge
+
+Parameters in response:
+    most are the same
+    -result_type is mixed (testing)
+    -max_results is 100 (testing, but really it should be kept like this)
+    -max_id field is at the end of the call, allowing each call of the function to retrieve older and older tweets
+
+time.sleep(1): Can only call the search api 180 times in 15 minutes, so ~5 seconds. Right now set to one because testing, but should probably be set to 10self.
+                Or, we can edit the make-twitter_request function to handle this error for us
+
+The try:except: is there because this is still unstable. I've encountered errors that I haven't yet figured outself.
+    -One big error is when next_results eventually is null, (there are no older tweets to retrieve)
+'''
+for i in range(1,10):
+    #print(i) #testing code
+    try:
+        response = make_twitter_request(twitter_api.search.tweets,q=q,result_type='mixed',max_results=100,until = '2018-03-29',max_id=maxid)
+        next_results = response['search_metadata']['next_results']
+        getMaxID  = dict([ kv.split('=') for kv in next_results[1:].split("&") ])
+        maxid = getMaxID['max_id']
+        time.sleep(1)
+    except:
+        json.dump(tweetsIngraham,ingrahamSearch)
+    print(maxid)
+    for tweet in response['statuses']:
+        tweetsIngraham[tweet['id']] = tweet
+    tweetsIngrahamList += str(tweetsIngraham)
+json.dump(tweetsIngraham,ingrahamSearch) #dumps list/dictionary to file. It can read in as a dictionary that works but I have no idea why. see test.py
---
+++ test.py
@@ -0,0 +1,22 @@
+import json
+import pprint
+from TwitterModule import *
+
+
+file3 = open('ingrahamSearch.json','r')
+largeFile = json.load(file3)
+file3.close()
+
+
+print("success")#successfulyl reads in the file
+
+print(type(largeFile)) #the type of the file. inexplicably it's a dict. It should be a list?
+
+'''
+Prints the contents of the file. Use with caution.
+'''
+#print json.dumps(largeFile, indent=1)
+
+
+print(len(largeFile))#length of the dictionary
+print(largeFile.keys())#the keys (twitter IDs) in the dictionary

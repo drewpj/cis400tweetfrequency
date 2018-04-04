e2886d9c1f3b700189cc37da91429a596055d65b
drewpj <drewjacobson@live.com>
2018-04-04 11:02:20 -0400

    updated test file to be able to test recently gathered data

--- test.py
+++ test.py
@@ -1,22 +1,31 @@
 import json
-import pprint
+#import pprint #only for fun printing
 from TwitterModule import *
 
+names = ['@IngrahamAngle','@davidhogg111','@sleepnumber','@ATT','@Allstate','@esurance','@Bayer','@RocketMortgage','@LibertyMutual','@Arbys','@TripAdvisor','@Nestle','@hulu','@Wayfair','@FoxNews','#BoycottIngramAdverts','#boycottLauraIngraham','#FireIngraham','#FireLauraIngraham']
+for q in names:
+    name = q[1:]
+    nameFile = name + '328.json'
+    file = open(nameFile,'r')
 
-file3 = open('ingrahamSearch.json','r')
-largeFile = json.load(file3)
-file3.close()
-
-
-print("success")#successfulyl reads in the file
-
-print(type(largeFile)) #the type of the file. inexplicably it's a dict. It should be a list?
-
-'''
-Prints the contents of the file. Use with caution.
-'''
-#print json.dumps(largeFile, indent=1)
+    #loads decodes json objects into dictionary
+    largeFile = json.load(file)
 
 
-print(len(largeFile))#length of the dictionary
-print(largeFile.keys())#the keys (twitter IDs) in the dictionary
+    print("successfully opened file " + q)#successfulyl reads in the file
+    print(type(largeFile)) #the type of the file. inexplicably it's a dict. It should be a list?
+
+
+    '''
+    Prints the contents of the file. Use with caution.
+    '''
+    #entire dictionary
+    #print json.dumps(largeFile, indent=1)
+
+    #length of the dictionary
+    print(len(largeFile))
+
+    #the keys (twitter IDs) in the dictionary
+    #print(largeFile.keys())
+
+    file.close()
